a9c61840ffd8b14d558130a931e3966daf99d82f
Pranjal Shukla <pranjal779@gmail.com>
2023-07-27 14:20:32 -0400

    more common ways

--- JavaScript Algorithms and Data Structures/commonStringsInJS.js
+++ JavaScript Algorithms and Data Structures/commonStringsInJS.js
@@ -2,3 +2,18 @@ const myStr1 = "This is the start. " + "Thi is the end.";
 
 let myStr2 = "This is the first sentence. ";
 myStr2 += "This is the second sentence.";
+
+// Constructing strings with Variables
+// Only change code below this line
+const myNameO = "Pranjal Shukl";
+const myStrO = "My name is " + myName + " and I am well!";
+
+
+// Appending Variables to Strings
+// Change code below this line
+const someAdjective = "Code" + "iz" + "de way";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
+
