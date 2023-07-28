45a364ca2bf57ea3b337e913268a02caee9e6c0d
Pranjal Shukla <pranjal779@gmail.com>
2023-07-27 20:09:29 -0400

    zero based indexing

---
+++ JavaScript Algorithms and Data Structures/zero-based indexing.js
@@ -0,0 +1,12 @@
+// use Bracket Notation to Find the First Character in a String
+
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Display...
+console.log(firstLetterOfLastName);
