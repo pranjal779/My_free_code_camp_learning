7b1e3845dea579dad6b780341573ff10f369e469
Pranjal Shukla <pranjal779@gmail.com>
2023-07-27 22:01:50 -0400

    lastTh character

---
+++ JavaScript Algorithms and Data Structures/lastTH-character.js
@@ -0,0 +1,11 @@
+// Use Bracket Notation to Find the last Character in a String
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// display...
+console.log(lastLetterOfLastName);
+
