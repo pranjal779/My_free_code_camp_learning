1f5ad6ea4ea9a116d97419cb92c8c15bcc7b6995
Pranjal Shukla <pranjal779@gmail.com>
2023-07-27 20:31:09 -0400

    nthPosition based indexing in JavaScript

---
+++ JavaScript Algorithms and Data Structures/nthPosition-based indexing.js
@@ -0,0 +1,10 @@
+// Use Bracket Notation to Find Nth Character in a String
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Display thirdLetterOfLastName.
+console.log(thirdLetterOfLastName);
