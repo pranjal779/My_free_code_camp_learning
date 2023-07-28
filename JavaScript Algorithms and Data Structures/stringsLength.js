3cdbecaa9d532f1320f2147a6b243a4315a8b061
Pranjal Shukla <pranjal779@gmail.com>
2023-07-27 20:01:11 -0400

    count the length of string variable or string literal

---
+++ JavaScript Algorithms and Data Structures/stringsLength.js
@@ -0,0 +1,11 @@
+// Find the length of a string
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+
+// displaying the length of lastNameLength variable.
+console.log(lastNameLength);
