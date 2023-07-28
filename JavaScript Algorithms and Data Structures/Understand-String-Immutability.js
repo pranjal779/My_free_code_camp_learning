2ce6252c48a5fe3bff98cdd2ad1e0e5592443b8b
Pranjal Shukla <pranjal779@gmail.com>
2023-07-27 20:22:45 -0400

    String-Immutability

---
+++ JavaScript Algorithms and Data Structures/Understand-String-Immutability.js
@@ -0,0 +1,16 @@
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr[0] = "H"; // Change this line
+// Only change code above this line
+/*
+the above line
+myStr[0] = "H";
+throws an error
+*/
+
+// change + display
+myStr = "Hello World"
+
+console.log(myStr);
