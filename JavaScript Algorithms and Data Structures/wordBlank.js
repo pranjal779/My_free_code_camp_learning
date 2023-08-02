804c99186ca489afe4ce3099d4191ef3eb084c57
Pranjal Shukla <pranjal779@gmail.com>
2023-08-02 11:30:03 -0400

    fill in the blanks

---
+++ JavaScript Algorithms and Data Structures/wordBlank.js
@@ -0,0 +1,11 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myNoun + " is " + myAdjective + " and one day, it " + myVerb + " so " + myAdverb + ", I was amazed."; // Change this line
+// Only change code above this line
+
+// output
+console.log(wordBlanks);
