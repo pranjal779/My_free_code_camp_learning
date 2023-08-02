584b838ad3001a440617c0a641a0bff8cb282c6b
Pranjal Shukla <pranjal779@gmail.com>
2023-08-02 11:49:28 -0400

    example of multi-dimensional array / Nested Array

---
+++ JavaScript Algorithms and Data Structures/nestedArray.js
@@ -0,0 +1,13 @@
+/*
+Nest one Array within Another Array
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array.
+*/
+
+// Only change code below this line
+const myArray = [["One Piece", 1, "Need to watch/start"], ["Naruto", 2, "Watched"]];
+
+// output
+console.log(myArray);
