fa7e16225619b6078be24b00806fa7e12892d57d
Pranjal Shukla <pranjal779@gmail.com>
2023-09-12 10:56:19 -0400

    Module 1

---
+++ FCC-Free-Foundational-C#-Certification-from-Microsoft–Full-Course/Write your first C# code/firstCode.cs
@@ -0,0 +1,5 @@
+// Console.WriteLine("Hello World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
---
+++ FCC-Free-Foundational-C#-Certification-from-Microsoft–Full-Course/Write your first C# code/task4-challenge.cs
@@ -0,0 +1,9 @@
+// first method
+Console.WriteLine("This is the first line.");
+
+// second method
+Console.Write("This");
+Console.Write(" is");
+Console.Write(" the");
+Console.Write(" second");
+Console.Write(" line.");
