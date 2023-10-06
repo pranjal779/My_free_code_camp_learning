61c312ffc8f0df2c407a59334f6bff991d108dab
Pranjal Shukla <pranjal779@gmail.com>
2023-10-05 20:54:57 -0400

    multiply-return-value
    Return a Value from a Function with Return

---
+++ JavaScript Algorithms and Data Structures/multiply-return-value.js
@@ -0,0 +1,25 @@
+function timesFive(num) {
+  return num * 5
+}
+
+const multipl = timesFive(8);
+
+console.log(multipl);
+
+/*
+Return a Value from a Function with Return
+We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
+Example
+
+function plusThree(num) {
+  return num + 3;
+}
+
+const answer = plusThree(5);
+answer has the value 8.
+
+plusThree takes an argument for num and returns a value equal to num + 3.
+
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+*/
