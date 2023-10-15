4ae653f34ad271bc1578fd157a19b12cf25237c2
Pranjal Shukla <pranjal779@gmail.com>
2023-10-15 17:35:26 -0400

    challenge02-02

---
+++ FCC-Free-Foundational-C#-Certification-Microsoft–Full-Course/Get started with C#, Part 1/Store and retrieve data using literal and variable values in C#/challenge02-02.cs
@@ -0,0 +1,5 @@
+string name = "Bob";
+int numb = 3;
+decimal numb2 = 34.4m;
+
+Console.WriteLine("Hello, " + name + "! You have " + numb + " message in your inbox. The temperature is " + numb2 + " celsius.");
