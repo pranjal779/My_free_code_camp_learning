879fc140c6638ed04f5226fe3237ef1d7445af64
Pranjal Shukla <pranjal779@gmail.com>
2023-10-15 17:28:37 -0400

    litteral-variable-challenge

---
+++ FCC-Free-Foundational-C#-Certification-Microsoft–Full-Course/Get started with C#, Part 1/Store and retrieve data using literal and variable values in C#/litteral-variable-challenge.cs
@@ -0,0 +1,5 @@
+var name = "Bob";
+var numb = 3;
+var numb2 = 34.4f;
+
+Console.WriteLine("Hello, " + name + "! You have " + numb + " message in your inbox. The temperature is " + numb2 + " celsius.");
