31a04217db64decdf798e305964b83ead48540eb
Pranjal Shukla <pranjal779@gmail.com>
2023-12-20 23:34:05 -0500

    bracketObjNotation codeSolution

--- JavaScript Algorithms and Data Structures/bracketObjNotation.js
+++ JavaScript Algorithms and Data Structures/bracketObjNotation.js
@@ -43,3 +43,30 @@ const playerNumber = 42;  // Change this line
 const player = testObj;   // Change this line
 */
 
+/*
+// Only change code below this line
+const playerNumber = 42;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+console.log(player);
+
+output:
+undefined
+*/
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+console.log(player);
+
+
+/*
+output:
+Montana
+*/
