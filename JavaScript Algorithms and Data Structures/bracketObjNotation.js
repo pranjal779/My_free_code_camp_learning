46fbdb98173fe839c78a66beb27557e9e8c09e00
Pranjal Shukla <pranjal779@gmail.com>
2023-12-20 23:22:08 -0500

    Update bracketObjNotation

--- JavaScript Algorithms and Data Structures/bracketObjNotation.js
+++ JavaScript Algorithms and Data Structures/bracketObjNotation.js
@@ -20,3 +20,26 @@ Note that we do not use quotes around the variable name when using it to access 
 Set the playerNumber variable to 16. Then, use the variable to look up the player's name and assign it to player.
 */
 
+/*
+Tests
+Waiting:playerNumber should be a number
+Waiting:The variable player should be a string
+Waiting:The value of player should be the string Montana
+Waiting:You should use bracket notation to access testObj
+Waiting:You should not assign the value Montana to the variable player directly.
+Waiting:You should be using the variable playerNumber in your bracket notation
+*/
+
+/*
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 42;  // Change this line
+const player = testObj;   // Change this line
+*/
+
