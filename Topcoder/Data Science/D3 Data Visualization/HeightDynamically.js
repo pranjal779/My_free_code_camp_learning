f0547fad9e73570786d9c72fe7dcf63cdd135fbd
Pranjal Shukla <pranjal779@gmail.com>
2024-01-09 20:02:28 -0500

    HeightDynamically

---
+++ Topcoder/Data Science/D3 Data Visualization/HeightDynamically.js
@@ -0,0 +1,30 @@
+// Update the Height of an Element Dynamically
+
+<style>
+  .bar {
+    width: 25px;
+    height: 100px;
+    display: inline-block;
+    background-color: blue;
+  }
+</style>
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+
+    d3.select("body").selectAll("div")
+      .data(dataset)
+      .enter()
+      .append("div")
+      .attr("class", "bar")
+      // Add your code below this line
+      .text((dataset) => dataset)
+      .style("height", (dataset) => dataset);
+
+
+
+
+      // Add your code above this line
+  </script>
+</body>
