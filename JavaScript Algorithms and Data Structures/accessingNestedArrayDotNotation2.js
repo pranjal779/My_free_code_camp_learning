19ce44d9dd824373a8d9c71a2ccb5bb6a29e26ed
Pranjal Shukla <pranjal779@gmail.com>
2024-01-17 11:04:36 -0500

    redid accessingNestedArrayDotNotation2

---
+++ JavaScript Algorithms and Data Structures/accessingNestedArrayDotNotation2.js
@@ -0,0 +1,33 @@
+/* redid the ex on 1/17/24 */
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+console.log(secondTree);
+
+/*
+Accessing Nested Arrays
+
+// running tests
+// tests completed
+// console output
+pine
+*/
