6346229d1d1771db5a2bf1cc9c9fdc200dd744b8
Pranjal Shukla <pranjal779@gmail.com>
2024-01-22 16:00:44 -0500

    generate random Decimals from a number

---
+++ JavaScript Algorithms and Data Structures/generateRandomFractionJS.js
@@ -0,0 +1,39 @@
+function randomFraction() {
+
+	   // Only change code below this line
+	
+	   return Math.random(0);
+
+	   // Only change code above this line
+	 
+	}
+console.log(Math.random(0));
+
+
+/*
+ *
+ * Generate Random Fractions with JavaScript
+ * Random numbers are useful for creating random behavior.
+ *
+ * JavaScript has a Math.random() function that generates a random decimal number between 0 (inclusive) and 1 (exclusive). Thus Math.random() can return a 0 but never return a 1.
+ *
+ * Note: Like Storing Values with the Assignment Operator, (https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/storing-values-with-the-assignment-operator ) all function calls will be resolved before the return executes, so we can return the value of the Math.random() function.
+ *
+ * Change randomFraction to return a random number instead of returning 0.
+ *
+ */
+
+/*
+ *
+ *Tests
+ *Passed:randomFraction should return a random number.
+ *Passed:The number returned by randomFraction should be a decimal.
+ *Passed:You should be using Math.random to generate the random decimal number.
+ *
+ *
+ *
+ *
+ * 0.7746822920912202
+ *
+ *
+ */
