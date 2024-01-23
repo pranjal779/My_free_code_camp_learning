fdc8d1b34853ca95f0012add9011721fbbeec811
Pranjal Shukla <pranjal779@gmail.com>
2024-01-23 14:09:36 -0500

    Create parseInt-fn

---
+++ JavaScript Algorithms and Data Structures/parseInt-fn.js
@@ -0,0 +1,34 @@
+function convertToInteger(str) {
+  const result = parseInt(str);
+
+  return result;
+}
+
+convertToInteger("56");
+
+console.log(convertToInteger("01100"));
+
+
+/*
+Use the parseInt Function
+The parseInt() function parses a string and returns an integer. Here's an example:
+
+const a = parseInt("007");
+The above function converts the string 007 to the integer 7. If the first character in the string can't be converted into a number, then it returns NaN.
+
+Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+
+*/
+
+/*
+Tests
+Passed:convertToInteger should use the parseInt() function
+Passed:convertToInteger("56") should return a number
+Passed:convertToInteger("56") should return 56
+Passed:convertToInteger("77") should return 77
+Passed:convertToInteger("JamesBond") should return NaN
+*/
+
+/*
+1100
+*/
