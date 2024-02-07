fcb12701469724c06d1c7963289d6c3c4bee9999
Pranjal Shukla <pranjal779@gmail.com>
2024-02-07 11:39:11 -0500

    step 30

---
+++ Scientific Computing with Python (Beta)/  Learn String Manipulation by Building a Cipher/step30.py
@@ -0,0 +1,24 @@
+'''
+Step 30
+At the end of your loop body, declare a variable called new_index and assign the value of index + shift to this variable.
+'''
+  
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
+
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"You rock!"
+
+32% complete
+Learn String Manipulation by Building a Cipher
+32% complete
+'''
