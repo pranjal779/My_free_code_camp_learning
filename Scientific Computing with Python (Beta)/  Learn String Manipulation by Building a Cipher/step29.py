bb49157fc5aaede0be63b64ef1b430692aad6803
Pranjal Shukla <pranjal779@gmail.com>
2024-02-07 11:35:32 -0500

    step 29

---
+++ Scientific Computing with Python (Beta)/  Learn String Manipulation by Building a Cipher/step29.py
@@ -0,0 +1,30 @@
+'''
+Step 29
+find is again returning -1 for uppercase letters, and for the space character, too. You are going to take care of the space later on.
+
+For now, instead of iterating over text, change the for loop to iterate over text.lower().
+'''
+
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+
+'''
+>>> h 7
+>>> e 4
+>>> l 11
+>>> l 11
+>>> o 14
+>>>   -1
+>>> w 22
+>>> o 14
+>>> r 17
+>>> l 11
+>>> d 3
+
+'''
