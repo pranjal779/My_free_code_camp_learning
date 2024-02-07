8ecd3bc5884c41d945c42845954b715501a40e45
Pranjal Shukla <pranjal779@gmail.com>
2024-02-07 10:14:05 -0500

    ES6 task 3
    Prevent Object Mutation

---
+++ JavaScript Algorithms and Data Structures/ES6/prevent_Object_Mutation.js
@@ -0,0 +1,69 @@
+// ES6 task 3
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  console.log(PI);
+  console.log(MATH_CONSTANTS);
+  // console.log("call line 1 freezeObj")
+  // console.log(freezeObj())
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+console.log(freezeObj())
+
+
+/*
+Output:
+undefined
+{ PI: 3.14 }
+[TypeError: Cannot assign to read only property 'PI' of object '#<Object>']
+3.14
+{ PI: 3.14 }
+[TypeError: Cannot assign to read only property 'PI' of object '#<Object>']
+3.14
+
+*/
+
+
+/*
+Prevent Object Mutation
+As seen in the previous challenge, const declaration alone doesn't really protect your data from mutation. 
+To ensure your data doesn't change, JavaScript provides a function Object.freeze to prevent data mutation.
+
+Any attempt at changing the object will be rejected, with an error thrown if the script is running in strict mode.
+
+let obj = {
+  name:"FreeCodeCamp",
+  review:"Awesome"
+};
+Object.freeze(obj);
+obj.review = "bad";
+obj.newProp = "Test";
+console.log(obj); 
+The obj.review and obj.newProp assignments will result in errors, because our editor runs in strict mode by default, 
+and the console will display the value { name: "FreeCodeCamp", review: "Awesome" }.
+
+In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. 
+You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
+*/
+
+/*
+Tests
+Passed:You should not replace the const keyword.
+Passed:MATH_CONSTANTS should be a constant variable (by using const).
+Passed:You should not change the original declaration of MATH_CONSTANTS.
+Passed:PI should equal 3.14.
+*/
