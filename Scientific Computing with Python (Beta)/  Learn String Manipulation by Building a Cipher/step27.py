fe54e41fd76b33c59e97249a184020fc80f6af88
Pranjal Shukla <pranjal779@gmail.com>
2024-02-07 11:27:24 -0500

    step 27

---
+++ Scientific Computing with Python (Beta)/  Learn String Manipulation by Building a Cipher/step27.py
@@ -0,0 +1,12 @@
+'''
+Step 27
+Inside the for loop, before printing the current character, declare a variable called index and assign the value returned by alphabet.find(char) to this variable.
+'''
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text:
+    index = alphabet.find(char)
+    print(char)
