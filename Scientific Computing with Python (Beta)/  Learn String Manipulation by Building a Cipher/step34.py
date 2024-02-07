aa7cee6ec3a24318467cec84404a87591f52b04d
Pranjal Shukla <pranjal779@gmail.com>
2024-02-07 12:35:27 -0500

    step 34

---
+++ Scientific Computing with Python (Beta)/  Learn String Manipulation by Building a Cipher/step34.py
@@ -0,0 +1,25 @@
+'''
+Step 34
+Now you need to create a new_char variable at the end of your loop body. Set its value to alphabet[new_index].
+'''
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"Spool up the FTL drive!"
+
+36% complete
+Learn String Manipulation by Building a Cipher
+36% complete
+Navigated to Step 34
+'''
