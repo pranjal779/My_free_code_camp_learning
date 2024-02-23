a4be96e7fc72dde54c2098d66177699718c074a6
Pranjal Shukla <pranjal779@gmail.com>
2024-02-23 17:45:27 -0500

    step 67

---
+++ Scientific Computing with Python (Beta)/  Learn String Manipulation by Building a Cipher/step67.py
@@ -0,0 +1,30 @@
+text = 'Hello Zaira'
+custom_key = 'python'
+
+def vigenere(message, key):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+    
+        # Append space to the message
+        if char == ' ':
+            encrypted_text += char
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    
+    return encrypted_text
+
+# Step 67
+# Call your function passing text and custom_key as the arguments. Store the return value of the function call in a variable called encryption.
+
+encryption = vigenere(text, custom_key)
