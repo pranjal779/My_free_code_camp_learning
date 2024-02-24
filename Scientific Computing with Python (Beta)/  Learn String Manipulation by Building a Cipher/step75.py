1b76a8c551a4dbb5bd7a4a564de7f3685639771c
Pranjal Shukla <pranjal779@gmail.com>
2024-02-24 15:51:50 -0500

    step 75

---
+++ Scientific Computing with Python (Beta)/  Learn String Manipulation by Building a Cipher/step75.py
@@ -0,0 +1,43 @@
+# Step 75
+# Update your comments too. Your second comment should say encode/decode in place of encode. Your third comment should say encrypted/decrypted in place of encrypted.
+
+text = 'Hello Zaira'
+custom_key = 'python'
+
+def vigenere(message, key, direction):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+    
+        # Append space to the message
+        if char == ' ':
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+    
+encryption = vigenere(text, custom_key, 1)
+print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
+
+# Output:
+# >>> wcesc mpgkh
+# >>> hello zaira
+
+# "Down the rabbit hole we go!"
+
+# 79% complete
+# Learn String Manipulation by Building a Cipher
+# 79% complete
