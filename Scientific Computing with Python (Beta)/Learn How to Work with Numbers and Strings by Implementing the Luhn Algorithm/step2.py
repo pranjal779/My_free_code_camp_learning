3d741ad3067ff2e1ab599cf28c8e1e7eb569959b
Pranjal Shukla <pranjal779@gmail.com>
2024-02-27 20:21:21 -0500

    step 2

---
+++ Scientific Computing with Python (Beta)/Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm/step2.py
@@ -0,0 +1,11 @@
+# Step 2
+# Replace the pass statement with a variable named card_number and a value of '4111-1111-4555-1142'.
+
+def main():
+    card_number = '4111-1111-4555-1142'
+
+# "Keep on trucking!"
+
+# 6% complete
+# Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm
+# 6% complete
