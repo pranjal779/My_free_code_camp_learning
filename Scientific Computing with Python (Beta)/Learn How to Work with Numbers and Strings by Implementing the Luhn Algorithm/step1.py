60b6779a0d9169d026cfd8c1844c83bb678d3cdd
Pranjal Shukla <pranjal779@gmail.com>
2024-02-27 20:19:48 -0500

    step 1

---
+++ Scientific Computing with Python (Beta)/Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm/step1.py
@@ -0,0 +1,13 @@
+# Step 1
+# In this project, you will implement the Luhn Algorithm. This algorithm is a formula to validate a variety of identification numbers.
+
+# Start by declaring a function called main. Use the pass keyword to avoid an error.
+
+def main():
+    pass
+
+# "Storm that castle!"
+
+# 3% complete
+# Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm
+# 3% complete
