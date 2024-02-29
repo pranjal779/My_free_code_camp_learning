db4b5d7009194162f002097c51cb6f4c0b6ae0b1
Pranjal Shukla <pranjal779@gmail.com>
2024-02-29 12:55:17 -0500

    step 29

---
+++ Scientific Computing with Python (Beta)/Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm/step29.py
@@ -0,0 +1,38 @@
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+
+
+# Step 29
+# Move the print call below the number reassignment.
+
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+            print(number)
+
+
+# "Ludicrous Speed! Go!"
+
+# 83% complete
+# Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm
+# 83% complete
+
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    verify_card_number(translated_card_number)
+
+main()
+
+# Output:
+# >>> 1
