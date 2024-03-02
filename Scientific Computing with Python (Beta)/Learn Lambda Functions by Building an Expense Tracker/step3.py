d3d2f0173b9c14b1f5b27175779dc9fd80c5232a
Pranjal Shukla <pranjal779@gmail.com>
2024-03-02 09:47:24 -0500

    step 3

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step3.py
@@ -0,0 +1,18 @@
+# Step 3
+# The expenses parameter of your add_expense function will be a list of expenses. 
+# You want to be able to add items at the end of your list. For that you can use the .append() list method:
+
+# my_list = [2, 4, 7]
+# my_list.append(3)
+# In the example above, after appending 3, my_list would be [2, 4, 7, 3]. 
+# Replace pass with a call to the .append() method on the expenses list. Don't pass any arguments to .append() for now.
+
+def add_expense(expenses, amount, category):
+    expenses.append()
+
+
+# "Off the charts!"
+
+# 7% complete
+# Learn Lambda Functions by Building an Expense Tracker
+# 7% complete
