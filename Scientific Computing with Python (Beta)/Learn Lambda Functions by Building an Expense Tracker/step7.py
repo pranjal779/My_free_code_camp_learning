9b215198710f858206b50c76abe6757aea96e17b
Pranjal Shukla <pranjal779@gmail.com>
2024-03-02 18:02:38 -0500

    step 7

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step7.py
@@ -0,0 +1,19 @@
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount, 'category': category})
+
+# Step 7
+# Inside the print_expenses function, create a for loop that iterates over each item in the expenses list. 
+# Use expense as the loop variable and move pass inside the loop body.
+
+def print_expenses(expenses):
+    for expense in expenses:
+        pass
+
+# "Challenge destroyed!"
+
+# 16% complete
+# Learn Lambda Functions by Building an Expense Tracker
+# 16% complete
+
+
+expenses = []
