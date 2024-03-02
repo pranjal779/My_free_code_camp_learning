d31dc86aa651a35197b4d683496ec3caeca438e1
Pranjal Shukla <pranjal779@gmail.com>
2024-03-02 17:58:01 -0500

    step 6

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step6.py
@@ -0,0 +1,21 @@
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount, 'category': category})
+
+# Step 6
+# Start by defining a function named print_expenses that takes one parameter expenses. 
+# This function will later be used to display each expense in your list.
+
+# Fill the body of your new function with a pass statement.
+
+
+def print_expenses(expenses):
+    pass
+
+# "Keep on trucking!"
+
+# 14% complete
+# Learn Lambda Functions by Building an Expense Tracker
+# 14% complete
+
+
+expenses = []
