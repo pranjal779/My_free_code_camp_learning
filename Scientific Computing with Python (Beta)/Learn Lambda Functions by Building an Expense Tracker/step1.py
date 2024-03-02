08ce14558c0d4ec6a8f5817bc10c0b3360e47f24
Pranjal Shukla <pranjal779@gmail.com>
2024-03-01 20:25:37 -0500

    step 1

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step1.py
@@ -0,0 +1,13 @@
+# Step 1
+# In this project, you're going to build a simple, yet functional expense tracker in Python.
+
+# Start by defining a function named add_expense that takes three parameters: expenses, amount and category. Use the pass keyword to fill the function body.
+
+def add_expense(expenses, amount, category):
+    pass
+
+# "Boom-shakalaka!"
+
+# 2% complete
+# Learn Lambda Functions by Building an Expense Tracker
+# 2% complete
