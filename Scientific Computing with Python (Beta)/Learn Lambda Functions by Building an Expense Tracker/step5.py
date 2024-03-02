ab6ba125f632af6df4f9b549e51a9008ac916fd0
Pranjal Shukla <pranjal779@gmail.com>
2024-03-02 17:56:08 -0500

    step 5

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step5.py
@@ -0,0 +1,15 @@
+# Step 5
+# Add another key-value pair to the dictionary you are appending to the expense list. 
+# Use the string 'category' as the key, and the category parameter as the value.
+
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount, 'category': category})
+
+# "You're crushing it!"
+
+# 11% complete
+# Learn Lambda Functions by Building an Expense Tracker
+# 11% complete
+
+
+expenses = []
