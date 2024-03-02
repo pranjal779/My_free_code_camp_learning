e130571458f50491676645798d22c65879b08a8e
Pranjal Shukla <pranjal779@gmail.com>
2024-03-01 20:27:39 -0500

    step 2

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step2.py
@@ -0,0 +1,18 @@
+def add_expense(expenses, amount, category):
+    pass
+
+
+# Step 2
+# A list in Python is a built-in data type that allows you to store many items in a single data structure. 
+# In Python, you create a list by putting items inside square brackets [], with each item separated from the following one by a comma.
+
+# numbers = [1, 2, 3, 4]
+# Create an empty list named expenses. You will use it to store each of your expenses.
+
+expenses = []
+
+# "Most triumphant!"
+
+# 5% complete
+# Learn Lambda Functions by Building an Expense Tracker
+# 5% complete
