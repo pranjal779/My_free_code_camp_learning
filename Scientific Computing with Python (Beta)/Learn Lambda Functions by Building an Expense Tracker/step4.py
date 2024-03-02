ccaaacf639f44ec2342ad2fcb4e7ee6ee301ed06
Pranjal Shukla <pranjal779@gmail.com>
2024-03-02 09:50:54 -0500

    step 4

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step4.py
@@ -0,0 +1,19 @@
+# Step 4
+# A dictionary is another built-in data type in Python. A dictionary is a collection of data in the form of key-value pairs. 
+# Dictionaries are defined with curly braces {} and they contain key-value pairs separated by commas. Each key is followed by a colon : and the value:
+
+# {'amount': 50.0, 'category': 'Food'}
+# In the example above, 'amount' and 'category' are keys, and 50.0 and 'Food' are their corresponding values.
+
+# Create a dictionary with a key 'amount' and value of the amount parameter and pass your new dictionary to the .append() call.
+
+def add_expense(expenses, amount, category):
+    expenses.append({
+        'amount': amount
+    })
+
+# "You're a wizard, Harry!"
+
+# 9% complete
+# Learn Lambda Functions by Building an Expense Tracker
+# 9% complete
