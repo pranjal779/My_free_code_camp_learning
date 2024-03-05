3d52531087ee33e1d0f52b5ae8f300b520a36605
Pranjal Shukla <pranjal779@gmail.com>
2024-03-04 21:24:23 -0500

    step 01

---
+++ Scientific Computing with Python (Beta)/Learn Python List Comprehension by Building a Case Converter Program/step01.py
@@ -0,0 +1,21 @@
+'''
+Step 1
+In this project, you are going to learn about list comprehensions in Python by building a program that can take a camelCase or PascalCase formatted string and convert that to a snake_case formatted string.
+
+List comprehensions in Python are a concise way to construct a list without using loops or the .append() method. Apart from being briefer, list comprehensions often run faster.
+
+Start defining a new function named convert_to_snake_case() that accepts a string named pascal_or_camel_cased_string as input. For now, add a pass statement inside the function.
+'''
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    pass
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"You're on fire!"
+
+5% complete
+Learn Python List Comprehension by Building a Case Converter Program
+5% complete
+'''
