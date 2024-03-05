c79bc8d23648094861c3307dcc9f8b0887d7cd65
Pranjal Shukla <pranjal779@gmail.com>
2024-03-04 21:30:13 -0500

    Create step03.py

---
+++ Scientific Computing with Python (Beta)/Learn Python List Comprehension by Building a Case Converter Program/step03.py
@@ -0,0 +1,23 @@
+'''
+Step 3
+Now that you have an empty list in place, you can start iterating through the input string and start converting each character to snake case.
+
+Use a for loop to iterate through the pascal_or_camel_cased_string. Make sure to name the target variable char which is short for character. For now, add a pass statement in the loop body.
+'''
+                                                                                                                             
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    
+    for char in pascal_or_camel_cased_string:
+        pass
+
+                                                                                                                             
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"King of the Pirates!"
+
+14% complete
+Learn Python List Comprehension by Building a Case Converter Program
+14% complete
+'''
