1391c552865654fbd7eb4c6fc652e57d0fce9f3e
Pranjal Shukla <pranjal779@gmail.com>
2024-03-04 21:26:12 -0500

    step 02

---
+++ Scientific Computing with Python (Beta)/Learn Python List Comprehension by Building a Case Converter Program/step02.py
@@ -0,0 +1,19 @@
+'''
+Step 2
+Now create a new list named snake_cased_char_list inside the function. You can use a set of empty square braces to create the new list.
+
+This list will hold the characters of the string after you have converted them to snake case.
+'''
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []   
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"You're a wizard, Harry!"
+
+9% complete
+Learn Python List Comprehension by Building a Case Converter Program
+9% complete
+'''
