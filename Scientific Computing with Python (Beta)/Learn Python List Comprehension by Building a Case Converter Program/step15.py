0653829ccdf4ba18737b595fef75770bb09e9741
Pranjal Shukla <pranjal779@gmail.com>
2024-03-09 16:39:31 -0500

    step 15

---
+++ Scientific Computing with Python (Beta)/Learn Python List Comprehension by Building a Case Converter Program/step15.py
@@ -0,0 +1,25 @@
+# Step 15
+# So far, in this project you have not seen any usage of list comprehensions whatsoever. 
+# Like you learned in the first step, list comprehensions are a more concise way of constructing lists in Python.
+
+# Begin the transition to comprehensions by commenting out all the lines of code inside the convert_to_snake_case() function. 
+# Don't delete them as they'll be helpful when you implement the logic using a list comprehension.
+
+# Remember to add the pass keyword to the function body to prevent the code from failing during the tests.
+
+
+    # return clean_snake_cased_string
+    pass
+def main():
+    print(convert_to_snake_case('aLongAndComplexString'))
+
+if __name__ == '__main__':
+    main()
+
+# Congratulations, your code passes. Submit your code to continue.
+
+# "Kool Aid Man says oh yeah!"
+
+# 68% complete
+# Learn Python List Comprehension by Building a Case Converter Program
+# 68% complete
