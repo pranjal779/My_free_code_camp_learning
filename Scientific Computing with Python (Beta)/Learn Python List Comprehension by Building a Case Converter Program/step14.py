1add727518bb46711a4fece2c93c47b3a0ec6384
Pranjal Shukla <pranjal779@gmail.com>
2024-03-09 16:00:44 -0500

    step 14

---
+++ Scientific Computing with Python (Beta)/Learn Python List Comprehension by Building a Case Converter Program/step14.py
@@ -0,0 +1,29 @@
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+        if char.isupper():
+            converted_character = '_' + char.lower()
+            snake_cased_char_list.append(converted_character)
+        else:
+            snake_cased_char_list.append(char)
+    snake_cased_string = ''.join(snake_cased_char_list)
+    clean_snake_cased_string = snake_cased_string.strip('_')
+
+    return clean_snake_cased_string
+
+def main():
+    print(convert_to_snake_case('aLongAndComplexString'))
+
+
+# Step 14
+# Replace pass with a call to the main() function inside the body of the if statement. 
+# You should see the given camel or pascal cased string converted to snake case upon execution.
+
+if __name__ == '__main__':
+    main()
+
+# "The town is now red!"
+
+# 64% complete
+# Learn Python List Comprehension by Building a Case Converter Program
+# 64% complete
