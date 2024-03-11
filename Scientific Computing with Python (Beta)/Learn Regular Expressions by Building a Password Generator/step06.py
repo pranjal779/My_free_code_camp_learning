698561e9a6f7bd88a5b1fb10681207bbe282944c
Pranjal Shukla <pranjal779@gmail.com>
2024-03-11 12:28:21 -0400

    step 06

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step06.py
@@ -0,0 +1,27 @@
+'''
+Step 6
+Your all_characters variable is a string formed by all lowercase and uppercase letters, all the 10 digits and several special characters.
+
+Just before it, add a comment saying Combine all characters.
+'''
+
+import string
+
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+# Combine all characters
+all_characters = letters + digits + symbols
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"Feel that need for speed!"
+
+8% complete
+Learn Regular Expressions by Building a Password Generator
+8% complete
+'''
