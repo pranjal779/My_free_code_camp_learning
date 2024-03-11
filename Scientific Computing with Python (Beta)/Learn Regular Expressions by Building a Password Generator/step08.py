73abd47e41aa23f25ea855c23c78c32ed50400af
Pranjal Shukla <pranjal779@gmail.com>
2024-03-11 12:38:25 -0400

    step 08

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step08.py
@@ -0,0 +1,27 @@
+'''
+Step 8
+It is a common convention to place import statements at the top of your code. And additionally, in case of multiple import statements, sort them in alphabetical order to improve readability.
+
+At the top of your code, import the random module.
+'''
+
+import random
+import string
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+# Combine all characters
+all_characters = letters + digits + symbols
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"Keep calm and code on!"
+
+11% complete
+Learn Regular Expressions by Building a Password Generator
+11% complete
+'''
