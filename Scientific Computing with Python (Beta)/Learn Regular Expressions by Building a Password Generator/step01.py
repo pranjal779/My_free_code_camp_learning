01627de7d4ebc8f01259779c4ef7c9e2393e5758
Pranjal Shukla <pranjal779@gmail.com>
2024-03-11 11:34:32 -0400

    step 01

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step01.py
@@ -0,0 +1,17 @@
+'''
+Step 1
+A Python module is a file containing code designed to perform specific tasks. The Python standard library contains many modules that you can import and use in your code. You can achieve this by using the import statement followed by the name of the module.
+
+Start this project by importing the string module.
+'''
+
+import string
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"You're a shooting star!"
+
+1% complete
+Learn Regular Expressions by Building a Password Generator
+1% complete
+'''
