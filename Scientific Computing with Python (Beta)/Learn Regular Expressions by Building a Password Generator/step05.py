2f3c573da659b4f7e5eee9b0dca672780ea139c6
Pranjal Shukla <pranjal779@gmail.com>
2024-03-11 12:21:58 -0400

    step 05

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step05.py
@@ -0,0 +1,24 @@
+'''
+Step 5
+Now declare a variable named all_characters and assign it the result of concatenating your existing variables.
+'''
+
+import string
+
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+# all_characters will take the result of concatenating possible characters
+all_characters = letters + digits + symbols
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"Legen - wait for it - dary!"
+
+7% complete
+Learn Regular Expressions by Building a Password Generator
+7% complete
+'''
