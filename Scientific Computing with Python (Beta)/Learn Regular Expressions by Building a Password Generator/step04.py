c24f7bc7288ce3a691dfc5b0de8e9f103d7df4d2
Pranjal Shukla <pranjal779@gmail.com>
2024-03-11 11:56:06 -0400

    step 04

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step04.py
@@ -0,0 +1,24 @@
+'''
+Step 4
+These three variables constitute the possible characters to choose from when generating the password.
+
+Just before them, add a comment saying Define the possible characters for the password.
+'''
+
+import string
+
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"That's the way it's done!"
+
+6% complete
+Learn Regular Expressions by Building a Password Generator
+6% complete
+'''
