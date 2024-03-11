5e9b902c61f08e65223926816ae7bc07d9505ef5
Pranjal Shukla <pranjal779@gmail.com>
2024-03-11 11:50:31 -0400

    step 03

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step03.py
@@ -0,0 +1,19 @@
+'''
+Step 3
+Declare two new variables named digits and symbols and assign them string.digits and string.punctuation, respectively.
+'''
+
+import string
+
+letters = string.ascii_letters
+digits, symbols = string.digits, string.punctuation
+
+'''
+Congratulations, your code passes. Submit your code to continue.
+
+"Checkmate!"
+
+4% complete
+Learn Regular Expressions by Building a Password Generator
+4% complete
+'''
