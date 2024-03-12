4862974dd51dafb0cdbb2b2fb3dda1781e943d2f
Pranjal Shukla <pranjal779@gmail.com>
2024-03-12 12:31:02 -0400

    step 15

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step15.py
@@ -0,0 +1,23 @@
+import secrets
+import string
+
+
+def generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    # Step 15
+    # At the bottom of your function, declare a password variable and assign an empty string to this variable.
+  
+    password = ""
+
+# "Standing ovation!"
+
+# 21% complete
+# Learn Regular Expressions by Building a Password Generator
+# 21% complete
