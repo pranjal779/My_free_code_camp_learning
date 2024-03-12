4203eac10897859aad0d4b7dad1a630d9fecf40a
Pranjal Shukla <pranjal779@gmail.com>
2024-03-12 11:41:50 -0400

    step 02

--- Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step02.py
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step02.py
@@ -1,6 +1,8 @@
 '''
 Step 2
-You can access the utilities defined inside the imported module through the dot notation. The dot notation works by appending a dot followed by the utility name to the module name. For example, here's how to access the ascii_lowercase constant:
+You can access the utilities defined inside the imported module through the dot notation. 
+The dot notation works by appending a dot followed by the utility name to the module name. 
+For example, here's how to access the ascii_lowercase constant:
 
 import string
 
