7ac815480aebebb6c40563d42f968c95185bd1e9
Pranjal Shukla <pranjal779@gmail.com>
2024-03-13 16:54:12 -0400

    step 16

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step16.py
@@ -0,0 +1,19 @@
+import secrets
+import string
+
+
+def generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+#   Step 16
+# Below your new variable, add a comment saying Generate password.
+  
+    password = ''
+    # Generate password
+    
