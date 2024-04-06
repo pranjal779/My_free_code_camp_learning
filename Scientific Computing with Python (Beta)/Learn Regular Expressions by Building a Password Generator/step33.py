e1bf425bd0b5ba57227903e46c7daf7284900065
Pranjal Shukla <pranjal779@gmail.com>
2024-04-06 11:53:44 -0400

    step 33

---
+++ Scientific Computing with Python (Beta)/Learn Regular Expressions by Building a Password Generator/step33.py
@@ -0,0 +1,40 @@
+import re
+import secrets
+import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, '')
+        ]        
+
+    return password
+    
+# new_password = generate_password(8)
+# print(new_password)
+
+# Step 33
+# To check that the generated password meets the required features, you are going to use the findall() function from the re module. 
+# It's similar to search but it returns a list with all the occurrences of the matched pattern.
+
+# Replace the search() call with findall() and check the output.
+
+"Terminated."
+
+# 46% complete
+# Learn Regular Expressions by Building a Password Generator
+# 46% complete
