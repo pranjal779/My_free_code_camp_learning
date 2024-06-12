43026283cce283b9f57a430cc2f8d3224084db1c
Pranjal Shukla <pranjal779@gmail.com>
2024-06-12 19:52:49 -0400

    Create step02.py

---
+++ Scientific Computing with Python (Beta)/Learn the Bisection Method by Finding the Square Root of a Number/step02.py
@@ -0,0 +1,18 @@
+# Step 2
+# Give the square_root_bisection function the following parameters:
+
+# square_target: The number for which you want to find the square root.
+# tolerance (optional): The acceptable difference between the square of the approximate root value and the actual target value (default is 1e-7). 
+# The tolerance 1e-7 implies that the solution will be accurate to within 0.0000001 of the true value and is a good default choice that balances accuracy and performance.
+# max_iterations (optional): The maximum number of iterations to perform (default is 100). If the method doesn't converge within this limit, you'll assume the solution is not found.
+
+def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+    pass
+
+# Congratulations, your code passes. Submit your code to continue.
+
+# "You can do this all day!"
+
+# 10% complete
+# Learn the Bisection Method by Finding the Square Root of a Number
+# 10% complete
