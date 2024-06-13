4d84f60ef403866fad91a0ecf1dfd5e680da59d6
Pranjal Shukla <pranjal779@gmail.com>
2024-06-12 20:01:58 -0400

    Create step05.py

---
+++ Scientific Computing with Python (Beta)/Learn the Bisection Method by Finding the Square Root of a Number/step05.py
@@ -0,0 +1,19 @@
+# Step 5
+# You'll create separate cases for when square_target is 0 or 1.
+
+# Begin by creating an if statement to check if square_target is equal to 1.
+
+def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+    if square_target < 0:
+        raise ValueError('Square root of negative number is not defined in real numbers')
+    if square_target == 1:
+        pass
+
+
+# Congratulations, your code passes. Submit your code to continue.
+
+# "Kool Aid Man says oh yeah!"
+
+# 24% complete
+# Learn the Bisection Method by Finding the Square Root of a Number
+# 24% complete
