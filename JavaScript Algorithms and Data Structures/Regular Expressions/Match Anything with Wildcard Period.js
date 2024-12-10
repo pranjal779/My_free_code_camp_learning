0e8fd5c89a74234a33706197bc72d7d809e324eb
Pranjal Shukla <pranjal779@gmail.com>
2024-12-10 11:54:39 +0530

    Regular Expressions task 07

--- JavaScript Algorithms and Data Structures/Regular Expressions/Match Anything with Wildcard Period.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Match Anything with Wildcard Period.js
@@ -3,7 +3,9 @@
 // Thinking of all words that match, say, a misspelling would take a long time. 
 // Luckily, you can save time using the wildcard character: .
 
-// The wildcard character . will match any one character. The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
+// The wildcard character . will match any one character. The wildcard is also called dot and period.
+ // You can use the wildcard character just like any other character in the regex.
+ // For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
 
 // let humStr = "I'll hum a song";
 // let hugStr = "Bear hug";
