96c34888ad269284e4a01cd1a76c02d28ba4ee61
Pranjal Shukla <pranjal779@gmail.com>
2025-08-20 22:27:31 +0530

    Create step03.py

---
+++ Scientific Computing with Python (Beta)/Learn Lambda Functions by Building an Expense Tracker/step03.py
@@ -0,0 +1,20 @@
+```
+Step 3
+Python has a handful of list methods. Such as methods for adding or removing list items.
+
+You can add an item to the end of a list using the append() method. For example:
+
+Example Code
+example_list = [4, 5, 6]
+
+example_list.append(7)
+example_list now is [4, 5, 6, 7].
+
+Try to use the append() method to add 3 to my_list. Then print the list.
+```
+
+my_list = [1, 2]
+
+my_list.append(3)
+
+print(my_list)
